03780ff3998bb9046c008f0807e603ad70311ddb
Connor Holly <connorholly11@gmail.com>
2019-10-23 17:32:10 -0400

    changed with cleaner

--- data/seeds/001-country.js
+++ data/seeds/001-country.js
@@ -1,13 +1,13 @@
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex("country")
-    .truncate()
-    .then(function() {
-      // Inserts seed entries
-      return knex("country").insert([
-        { country: "United States" },
-        { country: "China" },
-        { country: "Ghana" }
-      ]);
-    });
+  // // Deletes ALL existing entries
+  // return knex("country")
+  //   .truncate()
+  //   .then(function() {
+  //     // Inserts seed entries
+  return knex("country").insert([
+    { country: "United States" },
+    { country: "China" },
+    { country: "Ghana" }
+  ]);
+  // });
 };
