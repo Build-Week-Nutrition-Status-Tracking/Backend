e4c120d277b395cc28387eec10765490352914f3
Connor Holly <connorholly11@gmail.com>
2019-10-23 14:54:18 -0400

    ...

--- knexfile.js
+++ knexfile.js
@@ -22,19 +22,6 @@ module.exports = {
     //   }
     // }
   },
-  staging: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/users.db3"
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    }
-  },
   production: {
     client: "sqlite3",
     useNullAsDefault: true,
