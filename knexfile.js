0b21883e2f71a52bbed448a1b30206358e8fa385
Connor Holly <connorholly11@gmail.com>
2019-10-23 12:53:21 -0400

    redownloaded dependencies

--- knexfile.js
+++ knexfile.js
@@ -35,11 +35,6 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
-    // pool: {
-    //   afterCreate: (conn, done) => {
-    //     conn.run("PRAGMA foreign_keys = ON", done);
-    //   }
-    // }
   },
   production: {
     client: "pg",
