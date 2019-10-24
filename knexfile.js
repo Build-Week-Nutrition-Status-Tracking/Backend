d4bf3f127a4a024ee029ca88ebbbba6eba658019
Connor Holly <connorholly11@gmail.com>
2019-10-24 17:58:52 -0400

    seeds ran again

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
 // require("dotenv").config();
-// const pg = require("pg");
-// pg.defaults.ssl = true;
+const pg = require("pg");
+pg.defaults.ssl = true;
 
 module.exports = {
   development: {
@@ -28,9 +28,12 @@ module.exports = {
   },
   production: {
     client: "pg",
+    useNullAsDefault: true,
+
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "./data/migrations"
+      directory: "./data/migrations",
+      tableName: "knex_migrations"
     },
     seeds: {
       directory: "./data/seeds"
